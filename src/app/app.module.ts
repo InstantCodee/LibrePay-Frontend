9f2076ac3d5d3e8b51382df33d42d26f158dd57d
Felix <apolltoast98@gmail.com>
2021-01-03 12:42:34 +0100

    merge master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { PaymentComponent } from './payment/payment.component';
 import { QRCodeModule } from 'angularx-qrcode';
 import { PayComponent } from './pay/pay.component';
+import { RouterModule } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +15,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { CartComponent } from './cart/cart.component';
 import { PushNotificationsModule } from 'ng-push-ivy';
 import { ClipboardModule } from 'ngx-clipboard';
-import { environment } from '../environments/environment';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './dashboard/login/login.component';
 import { FormsModule } from '@angular/forms';
@@ -33,18 +33,17 @@ import { DashboardHeaderComponent } from './dashboard/header/header.component';
     DashboardComponent,
     LoginComponent,
     OverviewComponent,
-    DashboardHeaderComponent
+    DashboardHeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     QRCodeModule,
     HttpClientModule,
     AppRoutingModule,
-    SocketIoModule.forRoot(config),
     PushNotificationsModule,
     ClipboardModule,
-    FormsModule,
-    SocketIoModule.forRoot(config)
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
