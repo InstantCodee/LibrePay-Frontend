29d316a967c289b0242d7f756aed696eaa36ede6
Felix <apollotoast98@gmail.com>
2021-03-26 20:09:31 +0100

    add chart bg add chart dummy data

--- src/app/dashboard/chart/chart.component.ts
+++ src/app/dashboard/chart/chart.component.ts
@@ -8,15 +8,23 @@ import { EChartsOption } from 'echarts';
 })
 export class ChartComponent {
   chartOption: EChartsOption = {
+    color: ["#C61A4E"],
     xAxis: {
       type: 'category',
       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   },
+
+  tooltip: {
+    trigger: 'item',
+    showDelay: 0,
+    transitionDuration: 0.2
+  },
+
   yAxis: {
       type: 'value'
   },
   series: [{
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      data: [820, 932, 901, 934, 1290, 1330, 1320, 654, 68, 846, 987, 846, 874],
       type: 'line',
       smooth: true
   }]
